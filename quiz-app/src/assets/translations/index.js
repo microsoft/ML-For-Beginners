2ba45ed6922b8cbcf355e50ea3978c04b31931b0
Mansi Jain <mansijain1519@gmail.com>
2021-08-28 01:45:25 +0530

    Add option of zh-cn in js file

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -3,6 +3,7 @@ import en from './en.json';
 import tr from './tr.json';
 import fr from './fr.json';
 import ja from './ja.json';
+import zh-cn from './zh-cn.json';
 
 //export const defaultLocale = 'en';
 
@@ -10,7 +11,8 @@ const messages = {
 	en: en[0],
 	tr: tr[0],
 	fr: fr[0],
-	ja: ja[0]
+	ja: ja[0],
+                   zh-cn: zh-cn[0]
 };
 
 export default messages;
