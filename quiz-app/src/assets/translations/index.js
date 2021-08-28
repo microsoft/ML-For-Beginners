f8eef92e1f54bf1e8c43e5c81e34a29937f05b89
Mansi Jain <mansijain1519@gmail.com>
2021-08-28 16:02:29 +0530

    corrected

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -3,16 +3,16 @@ import en from './en.json';
 import tr from './tr.json';
 import fr from './fr.json';
 import ja from './ja.json';
-import zh-cn from './zh-cn.json';
+import zh - cn from './zh-cn.json';
 
 //export const defaultLocale = 'en';
 
 const messages = {
-	en: en[0],
-	tr: tr[0],
-	fr: fr[0],
-	ja: ja[0],
-                   zh-cn: zh-cn[0]
+    en: en[0],
+    tr: tr[0],
+    fr: fr[0],
+    ja: ja[0],
+    zh - cn: zh - cn[0]
 };
 
 export default messages;
