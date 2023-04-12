0b234c2c13130309d0c39ed58bc48d64ab2cdffd
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-04-13 04:22:27 +0600

    improved and readable

--- 6-NLP/2-Tasks/solution/bot.py
+++ 6-NLP/2-Tasks/solution/bot.py
@@ -1,11 +1,12 @@
 import random
 from textblob import TextBlob
 from textblob.np_extractors import ConllExtractor
+
 extractor = ConllExtractor()
 
-def main():   
+def main():
     print("Hello, I am Marvin, the friendly robot.")
-    print("You can end this conversation at any time by typing 'bye'")    
+    print("You can end this conversation at any time by typing 'bye'")
     print("After typing each answer, press 'enter'")
     print("How are you today?")
 
@@ -13,14 +14,15 @@ def main():
         # wait for the user to enter some text
         user_input = input("> ")
 
-        if user_input.lower() == "bye":            
+        if user_input.lower() == "bye":
             # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
             break
         else:
             # Create a TextBlob based on the user input. Then extract the noun phrases
-            user_input_blob = TextBlob(user_input, np_extractor=extractor)                        
-            np = user_input_blob.noun_phrases                                    
+            user_input_blob = TextBlob(user_input, np_extractor=extractor)
+            np = user_input_blob.noun_phrases
             response = ""
+
             if user_input_blob.polarity <= -0.5:
                 response = "Oh dear, that sounds bad. "
             elif user_input_blob.polarity <= 0:
@@ -37,7 +39,7 @@ def main():
             else:
                 response = response + "Can you tell me more?"
             print(response)
-    
+
     print("It was nice talking to you, goodbye!")
 
 # Start the program
